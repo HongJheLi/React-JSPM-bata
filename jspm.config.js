e2438e71b877ce89b13ad072d1d6f9ae082cc864
HongJheLi <ntnu2005@gmail.com>
2016-05-01 11:02:00 +0800

    update react version to 15.0.2.

--- jspm.config.js
+++ jspm.config.js
@@ -24,21 +24,28 @@ SystemJS.config({
         "github:*/*.json"
     ],
     map: {
+        "assert": "github:jspm/nodelibs-assert@0.2.0-alpha",
         "babel-plugin-transform-react-jsx": "npm:babel-plugin-transform-react-jsx@6.7.5",
         "buffer": "github:jspm/nodelibs-buffer@0.2.0-alpha",
         "core-js": "npm:core-js@1.2.6",
+        "domain": "github:jspm/nodelibs-domain@0.2.0-alpha",
         "events": "github:jspm/nodelibs-events@0.2.0-alpha",
         "fs": "github:jspm/nodelibs-fs@0.2.0-alpha",
+        "http": "github:jspm/nodelibs-http@0.2.0-alpha",
+        "https": "github:jspm/nodelibs-https@0.2.0-alpha",
         "net": "github:jspm/nodelibs-net@0.2.0-alpha",
         "path": "github:jspm/nodelibs-path@0.2.0-alpha",
         "plugin-babel": "npm:systemjs-plugin-babel@0.0.7",
         "process": "github:jspm/nodelibs-process@0.2.0-alpha",
-        "react": "npm:react@0.14.8",
+        "react": "npm:react@15.0.2",
         "react-dom": "npm:react-dom@0.14.8",
         "stream": "github:jspm/nodelibs-stream@0.2.0-alpha",
+        "string_decoder": "github:jspm/nodelibs-string_decoder@0.2.0-alpha",
         "systemjs-hot-reloader": "github:capaj/systemjs-hot-reloader@0.5.7",
         "tty": "github:jspm/nodelibs-tty@0.2.0-alpha",
-        "util": "github:jspm/nodelibs-util@0.2.0-alpha"
+        "url": "github:jspm/nodelibs-url@0.2.0-alpha",
+        "util": "github:jspm/nodelibs-util@0.2.0-alpha",
+        "zlib": "github:jspm/nodelibs-zlib@0.2.0-alpha"
     },
     packages: {
         "github:capaj/systemjs-hot-reloader@0.5.7": {
@@ -53,9 +60,34 @@ SystemJS.config({
                 "buffer-browserify": "npm:buffer@4.6.0"
             }
         },
+        "github:jspm/nodelibs-domain@0.2.0-alpha": {
+            "map": {
+                "domain-browserify": "npm:domain-browser@1.1.7"
+            }
+        },
+        "github:jspm/nodelibs-http@0.2.0-alpha": {
+            "map": {
+                "http-browserify": "npm:stream-http@2.3.0"
+            }
+        },
         "github:jspm/nodelibs-stream@0.2.0-alpha": {
             "map": {
                 "stream-browserify": "npm:stream-browserify@2.0.1"
+            }
+        },
+        "github:jspm/nodelibs-string_decoder@0.2.0-alpha": {
+            "map": {
+                "string_decoder-browserify": "npm:string_decoder@0.10.31"
+            }
+        },
+        "github:jspm/nodelibs-url@0.2.0-alpha": {
+            "map": {
+                "url-browserify": "npm:url@0.11.0"
+            }
+        },
+        "github:jspm/nodelibs-zlib@0.2.0-alpha": {
+            "map": {
+                "zlib-browserify": "npm:browserify-zlib@0.1.4"
             }
         },
         "npm:babel-code-frame@6.7.7": {
@@ -75,6 +107,11 @@ SystemJS.config({
             }
         },
         "npm:babel-messages@6.7.2": {
+            "map": {
+                "babel-runtime": "npm:babel-runtime@5.8.38"
+            }
+        },
+        "npm:babel-plugin-syntax-flow@6.5.0": {
             "map": {
                 "babel-runtime": "npm:babel-runtime@5.8.38"
             }
@@ -124,6 +161,12 @@ SystemJS.config({
                 "babel-runtime": "npm:babel-runtime@5.8.38"
             }
         },
+        "npm:browserify-zlib@0.1.4": {
+            "map": {
+                "pako": "npm:pako@0.2.8",
+                "readable-stream": "npm:readable-stream@2.1.2"
+            }
+        },
         "npm:buffer@4.6.0": {
             "map": {
                 "base64-js": "npm:base64-js@1.1.2",
@@ -145,6 +188,21 @@ SystemJS.config({
                 "ms": "npm:ms@0.7.1"
             }
         },
+        "npm:encoding@0.1.12": {
+            "map": {
+                "iconv-lite": "npm:iconv-lite@0.4.13"
+            }
+        },
+        "npm:fbjs@0.8.1": {
+            "map": {
+                "babel-plugin-syntax-flow": "npm:babel-plugin-syntax-flow@6.5.0",
+                "core-js": "npm:core-js@1.2.6",
+                "isomorphic-fetch": "npm:isomorphic-fetch@2.2.1",
+                "loose-envify": "npm:loose-envify@1.1.0",
+                "promise": "npm:promise@7.1.1",
+                "ua-parser-js": "npm:ua-parser-js@0.7.10"
+            }
+        },
         "npm:has-ansi@2.0.0": {
             "map": {
                 "ansi-regex": "npm:ansi-regex@2.0.0"
@@ -160,14 +218,33 @@ SystemJS.config({
                 "number-is-nan": "npm:number-is-nan@1.0.0"
             }
         },
+        "npm:isomorphic-fetch@2.2.1": {
+            "map": {
+                "node-fetch": "npm:node-fetch@1.5.1",
+                "whatwg-fetch": "npm:whatwg-fetch@1.0.0"
+            }
+        },
         "npm:loose-envify@1.1.0": {
             "map": {
                 "js-tokens": "npm:js-tokens@1.0.3"
             }
         },
-        "npm:react@0.14.8": {
+        "npm:node-fetch@1.5.1": {
             "map": {
-                "fbjs": "npm:fbjs@0.6.1"
+                "encoding": "npm:encoding@0.1.12",
+                "is-stream": "npm:is-stream@1.1.0"
+            }
+        },
+        "npm:promise@7.1.1": {
+            "map": {
+                "asap": "npm:asap@2.0.3"
+            }
+        },
+        "npm:react@15.0.2": {
+            "map": {
+                "fbjs": "npm:fbjs@0.8.1",
+                "loose-envify": "npm:loose-envify@1.1.0",
+                "object-assign": "npm:object-assign@4.0.1"
             }
         },
         "npm:readable-stream@2.1.2": {
@@ -191,9 +268,24 @@ SystemJS.config({
                 "readable-stream": "npm:readable-stream@2.1.2"
             }
         },
+        "npm:stream-http@2.3.0": {
+            "map": {
+                "builtin-status-codes": "npm:builtin-status-codes@2.0.0",
+                "inherits": "npm:inherits@2.0.1",
+                "readable-stream": "npm:readable-stream@2.1.2",
+                "to-arraybuffer": "npm:to-arraybuffer@1.0.1",
+                "xtend": "npm:xtend@4.0.1"
+            }
+        },
         "npm:strip-ansi@3.0.1": {
             "map": {
                 "ansi-regex": "npm:ansi-regex@2.0.0"
+            }
+        },
+        "npm:url@0.11.0": {
+            "map": {
+                "punycode": "npm:punycode@1.3.2",
+                "querystring": "npm:querystring@0.2.0"
             }
         }
     }
